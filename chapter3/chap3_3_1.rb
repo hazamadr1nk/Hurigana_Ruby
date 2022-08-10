a6bb0c93d3ac8394051c80a4d41d50c3bfc8b88f
hazamadr1nk <110225244+hazamadr1nk@users.noreply.github.com>
2022-08-10 19:31:12 +0900

    chapter3 を実装

---
+++ chapter3/chap3_3_1.rb
@@ -0,0 +1,6 @@
+wdays = ["Monday", "Tuesday", "Wendesday", "Thursday", "Friday"]
+puts wdays[1]
+wdays[1] = "Fire"
+puts wdays
+wdays.delete("Friday")
+puts wdays
---
+++ chapter3/chap3_4_1.rb
@@ -0,0 +1,4 @@
+wdays = ["Monday", "Tuesday", "Wendesday", "Thursday", "Friday"]
+wdays.each do |wday|
+    puts wday + "曜日"
+end
